4761a62a0da13ec449ffefcde00bbfc957276a4c
Michael Morgan <morgan.michael@me.com>
2019-11-21 11:29:38 -0800

    Updates

---
+++ precache-manifest.bf407248daaa10a05c4ed6e818434f29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c708bf373aed6806ffe3a9dedda65241",
+    "url": "/guitar-chords/index.html"
+  },
+  {
+    "revision": "3c689a05271ed4dcd214",
+    "url": "/guitar-chords/static/css/main.1bb520b7.chunk.css"
+  },
+  {
+    "revision": "245c922273be808abe24",
+    "url": "/guitar-chords/static/js/2.cb1dbc49.chunk.js"
+  },
+  {
+    "revision": "3c689a05271ed4dcd214",
+    "url": "/guitar-chords/static/js/main.dccea2dc.chunk.js"
+  },
+  {
+    "revision": "7223faa1f86a0878bc61",
+    "url": "/guitar-chords/static/js/runtime-main.19bdce06.js"
+  }
+]);
